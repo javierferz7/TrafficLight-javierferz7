e59901dd9683137955093739ca39ba2b45a89406
Javier Fernandez <138958604+javierferz7@users.noreply.github.com>
2023-10-13 20:44:19 +0000

    trafficligth

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [currentColor, setCurrentColor] = useState("red");
+  const [display, setDisplay] = useState("true");
+  let light = null
+
+
+  if (display === true) {
+    light = (
+      <div className="text-center">
+        <div className="stick"></div>
+        <div className="stopLight">
+         
+          <div
+            className={currentColor === "red" ? "red light glow" : "red light"}
+            onClick={() => setCurrentColor("red")}
+          ></div>
+
+          <div
+            className={
+              currentColor === "yellow" ? "yellow light glow" : "yellow light"
+            }
+            onClick={() => setCurrentColor("yellow")}
+          ></div>
+		   <div
+            className={
+              currentColor === "yellow" ? "yellow light glow" : "yellow light"
+            }
+            onClick={() => setCurrentColor("yellow")}
+          ></div>
+          <div
+            className={
+              currentColor === "green" ? "green light glow" : "green light"
+            }
+            onClick={() => setCurrentColor("green")}
+          ></div>
+        </div>
+        <button onClick={() => {setDisplay(false)}}>Add Purple Light</button>
+      </div>
+    );
+  }
+  else (
+light = (
+	<div className="text-center">
+	<div className="stick"></div>
+	<div className="stopLight">
+	
+	  <div
+		className={currentColor === "red" ? "red light glow" : "red light"}
+		onClick={() => setCurrentColor("red")}
+	  ></div>
+	  	  <div
+		className={
+		  currentColor === "yellow" ? "yellow light glow" : "yellow light"
+		}
+		onClick={() => setCurrentColor("yellow")}
+	  ></div>
+	  <div
+		className={
+		  currentColor === "green" ? "green light glow" : "green light"
+		}
+		onClick={() => setCurrentColor("green")}
+	  ></div>
+	   <div
+		className={
+		  currentColor === "purple" ? "purple light glow" : "purple light"
+		}
+		onClick={() => setCurrentColor("purple")}
+	  ></div>
+	</div>
+	<button onClick={() => setDisplay(true)}>Remove Purple Light</button>
+  </div>
+  )
+  )
+
+  return (
+	<div>
+		{light}
+	</div>
+  );
 };
 
 export default Home;
